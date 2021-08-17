775fbadfaaab55aac6d99021d95f206b5c24c592
Smokey-444 <85306561+Smokey-444@users.noreply.github.com>
2021-08-17 18:28:05 +0200

    1

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-const fs = require('fs')
-const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-const reactionRolesConfig = JSON.parse(fs.readFileSync('reactionroles.json' , 'utf8'))
-const prefix = 't!'
+
+bot.on('ready', () => {
+    console.log('Der Bot ist nun Online!')
 
     bot.user.setPresence({
         activity: {
@@ -11,7 +10,7 @@ const prefix = 't!'
             type: 'PLAYING',
         }
     })
-
+})
 
 bot.on('message', message => {
     let parts = message.content.split(" ");
@@ -72,6 +71,7 @@ bot.on('message', message => {
 
         message.channel.send(embed)
     }
+
     else if(message.content.includes('t!server')) {
         const embed = new Discord.MessageEmbed()
         .setColor('ff0000')
@@ -81,67 +81,6 @@ bot.on('message', message => {
 
         message.channel.send(embed)
     }
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-      });
-      
-      
-      
-      client.on("messageReactionAdd", async (reaction, user) => {
-        if(reaction.message.partial) reaction.fetch();
-        if(reaction.partial) reaction.fetch();
-        if(user.bot || !reaction.message.guild) return;
-      
-        for (let index = 0; index < reactionRolesConfig.reactions.length; index++) {
-          let reactionrole = reactionRolesConfig.reactions[index];
-      
-          if(reaction.message.id == reactionrole.message && reaction.emoji.name == reactionrole.emoji){
-            reaction.message.guild.members.cache.get(user.id).roles.add(reactionrole.role)
-          }
-        }
-      })
-      
-      client.on("messageReactionRemove", async (reaction, user) => {
-        if(reaction.message.partial) reaction.fetch();
-        if(reaction.partial) reaction.fetch();
-        if(user.bot || !reaction.message.guild) return;
-      
-        for (let index = 0; index < reactionRolesConfig.reactions.length; index++) {
-          let reactionrole = reactionRolesConfig.reactions[index];
-      
-          if(reaction.message.id == reactionrole.message && reaction.emoji.name == reactionrole.emoji && reaction.message.guild.members.cache.get(user.id).roles.cache.has(reactionrole.role)){
-            reaction.message.guild.members.cache.get(user.id).roles.remove(reactionrole.role)
-          }
-        }
-      })
-      
-      
-      
-      client.on('message', async (msg) => {
-        if(msg.author.bot || !msg.guild) return;
-        if(msg.content.startsWith('!createReactionRole') && msg.member.hasPermission('ADMINISTRATOR')){
-          var args = msg.content.split(' ');
-          if(args.length == 3){
-            var emoji = args[1];
-            var roleid = args[2]
-            var role = msg.guild.roles.cache.get(roleid);
-            if(!role){
-              msg.reply('die rolle gibt es nicht')
-              return;
-            } 
-            var embed = new Discord.MessageEmbed()
-            .setTitle('Klicke auf ' + emoji)
-            .setDescription('Klicke auf ' + emoji + " um die Rolle " + `<@&${role.id}>` + " zu bekomme oder sie zu entfernen");
-            var message = await msg.channel.send(embed)
-            message.react(emoji)
-            var toSave = {message: message.id, emoji: emoji,role: roleid}
-            reactionRolesConfig.reactions.push(toSave);
-            let data = JSON.stringify(reactionRolesConfig);
-            fs.writeFileSync('reactionroles.json', data);   
-           }
-        }
-      
-      })
 })
 
 bot.login(process.env.token)
